09e513038fe0cd6a05076322aea22ee510643658
David Ashford <david.ashford@soundunited.com>
2022-09-29 10:02:03 +0100

    matlab

---
+++ test.m
@@ -0,0 +1,5 @@
+%testing to see if the matlab file can be pulled to github repository and
+%then pulled back down with the .m file made at the end 
+a = 10; %first number 
+b = 20; %second number
+c = a + b; %summation 
